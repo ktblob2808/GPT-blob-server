9638da18ce38a2f92820c822f6f6febba76608c9
martinho <kit.hmk@gmail.com>
2025-03-14 19:57:30 +0800

    Manual changes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+// Load environment variables from .env file in the project root directory
+require("dotenv").config(); 
+
+// Import database connection
+require("./dao/dbConnect");
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'ai_blog',
+    database: process.env.DB_NAME || 'mysite',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   },
