a2dd3f0d936f1c7621c72739e9438881e097854e
martinho <kit.hmk@gmail.com>
2025-03-22 16:58:47 +0800

    ai gen code

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 // Load environment variables from .env file in the project root directory
 require("dotenv").config(); 
 
 // Import database connection
 require("./dao/dbConnect");
 
+// Import custom error classes
+const { NotFoundError, UnknownError } = require('./utils/ServiceError');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,7 +29,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(new NotFoundError());
 });
 
 // error handler
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err instanceof ServiceError ? err.toResponse() : new UnknownError().toResponse());
 });
 
 module.exports = app;
---
+++ utils/ServiceError.js
@@ -0,0 +1,54 @@
+class ServiceError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code;
+    }
+
+    toResponse() {
+        return {
+            error: {
+                message: this.message,
+                code: this.code
+            }
+        };
+    }
+}
+
+class UploadError extends ServiceError {
+    constructor(message = 'Upload Error', code = 400) {
+        super(message, code);
+    }
+}
+
+class ForbiddenError extends ServiceError {
+    constructor(message = 'Forbidden', code = 403) {
+        super(message, code);
+    }
+}
+
+class ValidationError extends ServiceError {
+    constructor(message = 'Validation Error', code = 422) {
+        super(message, code);
+    }
+}
+
+class NotFoundError extends ServiceError {
+    constructor(message = 'Not Found', code = 404) {
+        super(message, code);
+    }
+}
+
+class UnknownError extends ServiceError {
+    constructor(message = 'Unknown Error', code = 500) {
+        super(message, code);
+    }
+}
+
+module.exports = {
+    ServiceError,
+    UploadError,
+    ForbiddenError,
+    ValidationError,
+    NotFoundError,
+    UnknownError
+};
